e7dfe8a1efd2c9cb5d930d7902c8a6ea1255364c
Sandra Vilder <sandra.vilder@gmail.com>
2020-09-16 11:01:20 +0200

    gh-pages set up

--- src/api/plantae.js
+++ src/api/plantae.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://plantae-plants-search.herokuapp.com',
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': 'Accept, Content-Type',
