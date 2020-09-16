6eb7f50dae5c3a6dfda2a9b01da1f53ba35d6291
Sandra Vilder <sandra.vilder@gmail.com>
2020-09-16 11:30:08 +0200

    Updates

---
+++ precache-manifest.59f662640607531836d0813db5fc4bfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c77a47b006860eb5a9b5342e326a85d",
+    "url": "/plantae/index.html"
+  },
+  {
+    "revision": "2f0d2c0ecb8d60bea908",
+    "url": "/plantae/static/css/main.0d04f35f.chunk.css"
+  },
+  {
+    "revision": "007398b9f2ae133332d7",
+    "url": "/plantae/static/js/2.3f6b7aad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/plantae/static/js/2.3f6b7aad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f0d2c0ecb8d60bea908",
+    "url": "/plantae/static/js/main.90e79da5.chunk.js"
+  },
+  {
+    "revision": "a969db77e0d705fd643c",
+    "url": "/plantae/static/js/runtime-main.3d84f730.js"
+  }
+]);
